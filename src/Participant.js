e5508fbbbfa6a02193993d81fa0f6a0bc4b9952c
Michael Weedman <michael.weedman@gmail.com>
2019-11-12 14:57:54 -0700

    Participant list styling

--- src/Participant.js
+++ src/Participant.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Participant(prop) {
   return (
-    <div className="session-participant">
+    <div className="session-participant" key={prop.id}>
       <img src={prop.avatar} alt={prop.name} className="user-avatar" />
       <div className="participant-info">
         <h6 className="participant-name">{prop.name}</h6>
